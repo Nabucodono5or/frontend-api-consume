7af995ef6bf9e46bea999d3a06bf87cdf6a3b230
Jefferson Abreu <pereirajefferson06@gmail.com>
2022-01-18 14:06:43 -0300

    criado buttons: edit e remove

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,20 @@
 import "./card.css";
+import { BsTrash } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
 
 function Card() {
   return (
     <div className="card">
+      <div className="badge-remove">
+        <BsTrash />
+      </div>
+      <div className="badge-edit">
+        <AiOutlineEdit />
+      </div>
       <img
         className="image-card"
         src="https://images-na.ssl-images-amazon.com/images/I/51mqUvezh2L._SX348_BO1,204,203,200_.jpg"
+        alt="Imagem do livro"
       ></img>
       <div className="footer-card">
         <h2 className="card-title">Duna</h2>
