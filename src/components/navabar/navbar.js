a5899b033910aca548d80c5d7e91156f664fb620
Jefferson Abreu <pereirajefferson06@gmail.com>
2022-01-18 17:03:27 -0300

    criado link para home route and fix footer position

--- src/components/navabar/navbar.js
+++ src/components/navabar/navbar.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import "./navabar.css";
 
 function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar">
-      <h1 className="navbar-title">Livraria Nabucodonosor</h1>
+      <h1 className="navbar-title" onClick={() => navigate("/")}>
+        Livraria Nabucodonosor
+      </h1>
     </nav>
   );
 }
